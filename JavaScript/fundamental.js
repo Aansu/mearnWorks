59d5407073b922658b3189457e6620d9155b7463
Aansu <aansunirupamajacob94@gmail.com>
2021-06-10 10:57:45 +0530

    Adding javascript initial file

---
+++ JavaScript/fundamental.js
@@ -0,0 +1,10 @@
+console.log("Haai Aansu Nirupama..Welcome");
+
+
+var pname="Aansu Nirupama";
+var page=28;
+console.log("hai am" +pname+","+page+" years old");
+
+console.log(`haii i am ${pname} and i am ${page} years old`);
+console.log(typeof(page));
+console.log(typeof(pname));
---
+++ JavaScript/swapping.js
@@ -0,0 +1,12 @@
+var num1=10,num2=20;
+
+console.log(`Before Swapping ${num1} and ${num2}`);
+// var temp=num1;
+// num1=num2;
+// num2=temp;
+// or
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+
+console.log(`After Swapping ${num1} and ${num2}`);
